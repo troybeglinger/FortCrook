94ec0ee1525151b97145a9efff9e04d95d265d50
Troy Beglinger <troy@beglingerfineart.com>
2022-10-02 16:43:26 -0700

    updated memorial

--- memorial.php
+++ memorial.php
@@ -80,7 +80,7 @@
     <div id="collapse-9" class="accordion-collapse collapse row" aria-labelledby="heading-9" data-bs-parent="#lodgeAccordion">
     <div class="col-sm-3 mx-auto">
       <figure class="figure"><br>
-      <img src="images/fall-river-1.jpg" class="figure-img img-fluid img-thumbnail mx-auto" alt="Gene E. Sanford, PM">
+      <img src="images/square-and-compass-white-web.png" class="figure-img img-fluid img-thumbnail mx-auto" alt="Gene E. Sanford, PM">
       <figcaption class="figure-caption">Image Not Available</figcaption>
       </figure>
     </div>
