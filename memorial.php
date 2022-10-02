a674d68909d0e58d78a5299f3fdd9c5ca7045bee
Troy Beglinger <troy@beglingerfineart.com>
2022-10-02 13:10:39 -0700

    updated memorial wall

--- memorial.php
+++ memorial.php
@@ -63,12 +63,36 @@
     <div class="w-100 d-md-none"></div>
     <div class="accordion-body col-sm-8 mx-auto">
     <p><strong>Offices Held:</strong> Worshipful Master, Secretary, Marshal</p>
-    <p><strong>Masonic Honors:</strong> Anniversary Award, 25 Year</p>
-    <p><strong>Initiated:</strong> Feb. 17, 1977 at Alturas Canby Cross No. 248</p>
-    <p><strong>Passed:</strong> June 20, 1977 at Alturas Canby Cross No. 248</p>
-    <p><strong>Raised:</strong> July 11, 1977 at Alturas Canby Cross No. 248</p>
+    <p><strong>Masonic Honors:</strong> Anniversary Award (25 Years)</p>
+    <p><strong>Initiated:</strong> Feb. 17, 1977 at Alturas Lodge &#35;248</p>
+    <p><strong>Passed:</strong> June 20, 1977 at Alturas Lodge &#35;248</p>
+    <p><strong>Raised:</strong> July 11, 1977 at Alturas Lodge &#35;248</p>
     <p><strong>Entered to Rest:</strong> March 30, 2022</p>
       </div>
+    </div>
+  </div>
+
+  <h2 class="accordion-header" id="heading-9">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-9" aria-expanded="false" aria-controls="collapse-9">
+      Gene E. Sanford, PM (1937-2020)
+      </button>
+    </h2>
+    <div id="collapse-9" class="accordion-collapse collapse row" aria-labelledby="heading-9" data-bs-parent="#lodgeAccordion">
+    <div class="col-sm-3 mx-auto">
+      <figure class="figure"><br>
+      <img src="images/fall-river-1.jpg" class="figure-img img-fluid img-thumbnail mx-auto" alt="Gene E. Sanford, PM">
+      <figcaption class="figure-caption">Image Not Available</figcaption>
+      </figure>
+    </div>
+<div class="w-100 d-md-none"></div>
+    <div class="accordion-body col-sm-8 mx-auto">
+    <p>Offices Held: Worshipful Master</p>
+    <p>Masonic Honors: Anniversary Award (25 Years)</p>
+    <p>Initiated: June 17, 1966 at Fort Crook Lodge &#35;575</p>
+    <p>Passed: August 16, 1966 at Fort Crook Lodge &#35;575</p>
+    <p>Raised: September 17, 1966 at Fort Crook Lodge &#35;575</p>
+    <p>Entered to Rest: December 21, 2020</p>
+        </div>
     </div>
   </div>
 
